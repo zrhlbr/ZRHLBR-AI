35710188d4fef9e2fb5490e562f6fad77107b753
ZRHLBR AI <zrhlbr-ai@users.noreply.github.com>
2026-08-17 20:01:36 +0630

    fix(ios): registration pending approval flow

---
+++ ios/App/App/public/assets/swimlanesDiagram-G3AALYLV-BPN-GbO0.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U-UCKT4BCs.js";import{_ as a}from"./mermaid.core-CQJGVRzc.js";import"./chunk-5VM5RSS4-DS2NsNQ0.js";import"./chunk-XXDRQBXY-CEFJytcy.js";import"./chunk-VR4S4FIN-Ckdo8ra7.js";import"./chunk-32BRIVSS-CQfoJQR6.js";import"./channel-6ahf5-lB.js";import"./index-B4ofkZm4.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
